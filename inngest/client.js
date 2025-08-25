075ea06ed0dc55fad7617a30e46e067017feb313
Karan757 <karanbodkhe757@gmail.com>
2025-08-26 00:55:47 +0530

    Update client.js

--- inngest/client.js
+++ inngest/client.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 
+// Create a client to send and receive events
 export const inngest = new Inngest({
-  id: "ai-lms-2", // This will become your app name in Inngest
-  signingKey: "signkey-prod-5188467046154d44b04311728d6e29c10c16076234f8ca14e7bbfa3f3ffe0e02",
+    id: "ai-lms-v2",
+    signingKey: process.env.INNGEST_SIGNING_KEY
 });
