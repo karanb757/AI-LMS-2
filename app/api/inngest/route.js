31b45baeea32ef68267005f5d0a5acfb80a43c12
Karan757 <karanbodkhe757@gmail.com>
2025-08-26 00:36:57 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,18 +1,16 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { 
-  helloWorld, 
-  CreateNewUser, 
-  GenerateNotes, 
-  GenerateStudyTypeContent 
-} from "../../../inngest/function";
+
+// Start with just one simple function for testing
+const testFunction = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    return { message: `Hello ${event.data.email}!` };
+  }
+);
 
 export const { GET, POST, PUT } = serve({
-  client: inngest, // This already has the hardcoded signingKey
-  functions: [    // ← Add your functions here!
-    helloWorld,
-    CreateNewUser, 
-    GenerateNotes,
-    GenerateStudyTypeContent
-  ],
+  client: inngest,
+  functions: [testFunction], // Start with just one function
 });
