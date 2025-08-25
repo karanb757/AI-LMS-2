576263a9ac7eef4500d3209e5732da0814c26a54
Karan757 <karanbodkhe757@gmail.com>
2025-08-26 00:51:51 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import {CreateNewUser, GenerateNotes, helloWorld,GenerateStudyTypeContent} from '../../../inngest/function.js'
+import {CreateNewUser, GenerateNotes,GenerateStudyTypeContent} from '../../../inngest/function.js'
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   streaming:'allow',
   functions: [
-    helloWorld,
     CreateNewUser,
     GenerateNotes,
     GenerateStudyTypeContent
